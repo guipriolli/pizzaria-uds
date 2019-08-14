6da67430793e8512e9a894f79b4d070c1f877c90
Guilherme Priólli Daniel <guipriollidaniel@gmail.com>
2019-08-14 20:07:15 -0300

    Estrutura inicial do projeto

---
+++ src/main/java/br/com/uds/pizzaria/Application.java
@@ -0,0 +1,16 @@
+package br.com.uds.pizzaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author Guilherme
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
